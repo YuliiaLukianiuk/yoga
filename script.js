4d304bd96286694cb04129ef07e22b0d19e2e88f
YuliiaLukianiuk <lukianiky@gmail.com>
2020-12-20 16:39:33 +0200

    Calc

--- script.js
+++ script.js
@@ -136,5 +136,49 @@ window.addEventListener('DOMContentLoaded', function(){
             currentSlide(i);
           }
         }
-      })
+      });
+
+      //Calc
+      let persons = document.querySelectorAll('.counter-block-input')[0],
+          restDays = document.querySelectorAll('.counter-block-input')[1],
+          totalValue = document.getElementById('total'),
+          place = document.getElementById('select'),
+          personsSum = 0,
+          daysSum = 0,
+          total = 0;
+
+          totalValue.innerHTML = 0;
+
+          persons.addEventListener('change', function() {
+            this.value = this.value.replace(/[\D]/g, "");
+            personsSum = +this.value;
+            total = (daysSum + personsSum) * 4000;
+
+            if(restDays.value == '' || persons.value == '') {
+              totalValue.innerHTML = 0;
+            }else {
+              totalValue.innerHTML = total;
+            }
+          });
+
+          restDays.addEventListener('change', function() {
+            this.value = this.value.replace(/[\D]/g, "");
+            daysSum = +this.value;
+            total = (daysSum + personsSum) * 4000;
+
+            if(persons.value == '' || restDays.value == '') {
+              totalValue.innerHTML = 0;
+            }else {
+              totalValue.innerHTML = total;
+            }
+          });
+
+          place.addEventListener('change', function() {
+            if (restDays.value == '' || persons.value == '') {
+              totalValue.innerHTML = 0;
+            }else{
+              let a = total;
+              totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+            }
+          });
 });
